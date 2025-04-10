6963ef213dda5b06616ac1011e2635f59ab91958
jdslater17 <jdslater17@gmail.com>
2025-04-10 18:11:49 -0400

    Update gradebook.js

--- gradebook.js
+++ gradebook.js
@@ -1,1 +1,12 @@
+//  TODO: Fetch data from the PostgreSQL database (to be implemented later)
+function fetchGradeData() {
+  console.log("Fetching grade data...");
+}
 
+// TODO: Populate the table with grade data
+function populateGradebook(data) {
+  console.log("Populating gradebook with data:", data);
+}
+const gradeData = fetchGradeData();
+populateGradebook(gradeData);
+// END REMOVE
