decc8915c47db0939c494524b98dec4ecc5d8630
jdslater17 <jdslater17@gmail.com>
2025-04-10 16:47:28 -0400

    Create gradebook.js

---
+++ gradebook.js
@@ -0,0 +1,8 @@
+function fetchGradeData() {
+  console.log("Fetching grade data...")
+}
+function populateGradebook(data) {
+  console.log("populate gradebook with data:", data);
+}
+const gradeData = fetchGradeData();
+populateGradebook(gradeData);
